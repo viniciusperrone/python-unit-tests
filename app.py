8545fe40dea6206e8bc76a287a1b95453991b158
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-01-24 22:20:13 -0300

    feat: added classificar_number function and test_classificar_number_grande test function

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ def par_ou_impar(num: int) -> str:
             return 'par'
         case 1:
             return 'impar'
+
+def classificar_number(num: int): 
+    if num < 0:
+        return 'negativo'
+    elif num == 0:
+        return 'zero'
+    elif 11 <= num <= 100:
+        return 'médio'
+    else:
+        return 'grande'
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from app import par_ou_impar
+from app import par_ou_impar, classificar_number
 
 def test_par_ou_impar_devolve_par():
     # Arrange / SetUp
@@ -7,6 +7,32 @@ def test_par_ou_impar_devolve_par():
 
     # Act / Exercise
     result = par_ou_impar(
+        num=num
+    )
+
+    # Assert / Verify
+    assert result == expected_result
+
+def test_par_ou_impar_devolve_impar():
+    # Arrange / SetUp
+    num = 501
+    expected_result = 'impar'
+
+    # Act / Exercise
+    result = par_ou_impar(
+        num=num
+    )
+
+    # Assert / Verify
+    assert result == expected_result
+
+def test_classificar_number_grande():
+    # Arrange / SetUp
+    num = 150
+    expected_result = 'grande'
+
+    # Act / Exercise
+    result = classificar_number(
         num=num
     )
 
